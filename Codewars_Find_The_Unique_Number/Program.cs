63dfa01e67f74d4fe0d799fc144b458f2c632c8d
Hajk <tunczorx@gmail.com>
2018-10-10 23:14:13 +0200

    Codewars task number 8. Description inside.

--- Codewars_Find_The_Unique_Number/Program.cs
+++ Codewars_Find_The_Unique_Number/Program.cs
@@ -10,6 +10,17 @@ namespace Codewars_Find_The_Unique_Number
     {
         static void Main(string[] args)
         {
+            //MySolution
+            int GetUnique(IEnumerable<int> numbers)
+            {
+                List<int> numbersList = numbers.ToList();
+                numbersList.Sort();
+                if (numbersList[0] != numbersList[1])
+                {
+                    return numbersList[0];
+                }
+                return numbersList[numbersList.Count - 1];
+            }
         }
     }
 }
